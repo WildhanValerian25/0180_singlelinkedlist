9693d67bd160739c5d06c7c682f38d760a13b5d7
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-05-08 10:38:02 +0700

    Menambahkan fungsi IF While

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -9,4 +9,49 @@ public:
     Node *next;
 };
 
+class singleLinkedList
+{
+    Node *START;
 
+public:
+    singleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if ((START == NULL) || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current = NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+      
